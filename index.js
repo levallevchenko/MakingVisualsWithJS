037d804b9e1d8604964b6fb1049db5e6aa5459d9
Anna Levchenko <levallevchenko@gmail.com>
2022-01-22 01:34:56 +0300

    feat: add array of squares

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 context.lineWidth = 4;
-context.beginPath();
-context.rect(100, 100, 400, 400);
-context.stroke();
 
-context.beginPath();
-context.arc(300, 300, 100, 0, Math.PI * 2);
-context.stroke();
+const WIDTH = 60;
+const HEIGH = 60;
+const GAP = 20;
+const SQUARES_COUNT = 5;
+let x, y;
+
+const squaresArray = new Array(SQUARES_COUNT).fill();
+
+const getSquare = (x, y, width, height) => {
+  context.beginPath();
+  context.rect(x, y, width, height);
+  context.stroke();
+}
+
+const getSquares = () => {
+  squaresArray.forEach((_, i) => {
+    squaresArray.forEach((_, j) => {
+      x = 100 + (WIDTH + GAP) * i;
+      y = 100 + (HEIGH + GAP) * j;
+
+      getSquare(x, y, WIDTH, HEIGH);
+      (Math.random() < 0.3) && getSquare(x + 8, y + 8, WIDTH - 16, HEIGH - 16);
+    });
+  });
+};
+
+
+setInterval(() => {
+  getSquares();
+}, 1000);
